22531641391b38abbd16fe141b04cdc7cafaebcc
Levi Whalen <levi@leviwhalen.com>
2022-09-14 23:12:42 -0400

    updated file

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -5,7 +5,7 @@
 import * as Tone from 'tone'
 const play = async () => {
     const player = new Tone.Player({
-        url: "rain-sounds.mp3",
+        url: "https://cdabf9b7b8ceefdf343dfe261084d747.r2.cloudflarestorage.com/leviwhalen/rain-sounds.mp3",
         loop: true,
         autostart: false,
     }).toDestination()
