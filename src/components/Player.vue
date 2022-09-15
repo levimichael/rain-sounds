da412f0512fef141962021298b8551b6e777cbaf
Levi Whalen <levi@leviwhalen.com>
2022-09-14 23:06:10 -0400

    basics so we can sleep

---
+++ src/components/Player.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="play">Play</button>
+</template>
+<script setup lang="ts">
+import * as Tone from 'tone'
+const play = async () => {
+    const player = new Tone.Player({
+        url: "rain-sounds.mp3",
+        loop: true,
+        autostart: false,
+    }).toDestination()
+
+    Tone.loaded().then(() => {
+        player.start();
+    });
+}
+
+</script>
